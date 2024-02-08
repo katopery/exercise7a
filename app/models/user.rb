300a74916f7f2d2e81927041b16894ac13b6e278
MasatoKato <152849350+katopery@users.noreply.github.co>
2024-02-08 13:05:27 +0000

    [Finish]　応用課題8a

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,10 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
+  
+  has_many :entries, dependent: :destroy
+  has_many :messages, dependent: :destroy
+  
   has_one_attached :profile_image
   
   #フォローしている関連付け
